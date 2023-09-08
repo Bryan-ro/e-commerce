376ac6e1a458bfba3de498d962c2f4d86f5604f0
Bryan-ro <bryangomesrocha@gmail.com>
2023-09-08 00:42:29 -0300

    Delete user

--- src/controllers/users/UserManager.controller.ts
+++ src/controllers/users/UserManager.controller.ts
@@ -5,8 +5,6 @@ import { isUserAlreadyExists } from "../../middlewares/users/universal/create/is
 import { isValidData } from "../../middlewares/users/universal/create/isValidData.middleware";
 import { isLoggedIn } from "../../middlewares/login/isLoggedIn.middleware";
 import { isManager } from "../../middlewares/users/roles/isManager.middleware";
-import { isNotOwnUser } from "../../middlewares/users/roles/isNotOwnUser.middleware";
-import { isUserExists } from "../../middlewares/users/universal/get/isUserExists.middleware";
 
 const service = new UserManagerService();
 const router = Router();
@@ -15,7 +13,6 @@ export class UserManagerController {
     public routes (): Router {
         router.get("/get-managers", isLoggedIn, isManager, this.getManagers);
         router.post("/create", isValidData, isUserAlreadyExists, this.create);
-        router.delete("/delete/:id", isLoggedIn, isManager, isUserExists, isNotOwnUser, this.delete);
         
         return router;
     }
@@ -32,14 +29,6 @@ export class UserManagerController {
         const creation = await service.create(user);
 
         return res.status(creation?.statusCode ?? 200).json({ ...creation });
-    }
-
-    private async delete (req: Request, res: Response) {
-        const id = Number(req.params.id);
-
-        const deleteUser = await service.deleteManager(id);
-
-        return res.status(deleteUser.statusCode).json({ ...deleteUser });
     }
 }
 
