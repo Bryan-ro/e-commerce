86f53664a402cd402656ae8c63d7fe5e58f41bac
Bryan-ro <bryangomesrocha@gmail.com>
2023-10-17 23:35:43 -0300

    mercadopago and order service creation-

--- src/services/orders/Order.service.ts
+++ src/services/orders/Order.service.ts
@@ -1,9 +1,17 @@
-import { OrderItem, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { CreateOrderDto } from "../../dto/order/CreateOrderDto";
+import { deliveryPriceCalculator } from "../../utils/deliveryPriceCalculator";
+import { AppError } from "../../errors/AppError";
+import mercadopago from "mercadopago";
+import "dotenv/config";
 
 const prisma = new PrismaClient();
 
-class OrderService {
+mercadopago.configure({
+    access_token: `${process.env.MERCADOPAGO_SECRET}`
+});
+
+export class OrderService {
     public async createOrder (data: CreateOrderDto) {
         const orderItens = [];
 
@@ -13,6 +21,7 @@ class OrderService {
                     id: data.orderItem[i].productId
                 }
             });
+            
             if(product) {
                 orderItens.push({
                     title: product.title,
@@ -20,9 +29,29 @@ class OrderService {
                     productId: product.id,
                     quantity: data.orderItem[i].quantity
                 });
-            }
-                
+            }        
         }
+
+        const address = await prisma.address.findUnique({
+            where: {
+                id: data.addressId
+            }
+        });
+
+        if(!address) {
+            throw new AppError("Error, address not found", 400);
+        }
+
+        let productQuantity = 0; 
+        let productAmount = 0;
+
+        orderItens.map((orderItem) => {
+            productQuantity = productQuantity + orderItem.quantity;
+            productAmount = productAmount + (orderItem.unitPrice * orderItem.quantity);
+        });
+
+    
+        const deliveryPrice = Number(await deliveryPriceCalculator(`${process.env.ORIGIN_DELIVERY_ZIPCODE}`, address.cep, productQuantity));
 
         const order = await prisma.order.create({
             data: {
@@ -31,8 +60,13 @@ class OrderService {
                         data: orderItens
                     }
                 },
-                totalAmount: 
-                
+                deliveryPrice: deliveryPrice,
+                totalAmount: productAmount + deliveryPrice,
+                deliveryStatus: "PREPARING",
+                paymentStatus: "PENDING",
+                address: {
+                    create: address
+                }
             }
         });
     }
--- src/utils/deliveryPriceCalculator.ts
+++ src/utils/deliveryPriceCalculator.ts
@@ -1,12 +1,22 @@
 import maps from "@google/maps";
 import "dotenv/config";
+import { AppError } from "../errors/AppError";
 
 const client = maps.createClient({
     key: `${process.env.GOOGLE_MAPS_KEY}`
 });
 
-export function deliveryPriceCalculator(senderZipCode: string, destinationZipCode: string, productQuantity: number) {
-    client.distanceMatrix({
-        
+export function deliveryPriceCalculator(senderZipCode: unknown, destinationZipCode: unknown, productQuantity: number) {
+    return new Promise((resolve) => {
+        client.distanceMatrix({
+            origins: senderZipCode as maps.LatLng[],
+            destinations: destinationZipCode as maps.LatLng[],
+        }, (err, result) => {
+            if(err) {
+                throw new AppError("Error to calculate the delivery price", 500);
+            }
+            
+            resolve(+result.json.rows[0].elements[0].distance.text.replace("km", "") * productQuantity);
+        });
     });
 }
